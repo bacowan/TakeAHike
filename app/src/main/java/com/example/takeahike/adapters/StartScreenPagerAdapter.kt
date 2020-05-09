7a5b9c3bd3612ca81cd58cbdeea99cfe91dbdca2
bacowan <bacowan@ualberta.ca>
2020-05-09 16:33:21 -0300

    yay the backstack works

--- app/src/main/java/com/example/takeahike/adapters/StartScreenPagerAdapter.kt
+++ app/src/main/java/com/example/takeahike/adapters/StartScreenPagerAdapter.kt
@@ -5,18 +5,16 @@ import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.takeahike.R
-import com.example.takeahike.ui.CurrentHike
-import com.example.takeahike.ui.EditRoutesList
 
-class StartScreenPagerAdapter(val fm: FragmentActivity) : FragmentStateAdapter(fm) {
+/*class StartScreenPagerAdapter(val fm: FragmentActivity) : FragmentStateAdapter(fm) {
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return if (position == 0) {
-            NavHostFragment.create(R.navigation.current_hike_graph)
+            NavHostFragment.create(R.navigation.nav_graph)
         }
         else {
             NavHostFragment.create(R.navigation.edit_routes_graph)
         }
     }
-}
+}*/
--- app/src/main/java/com/example/takeahike/ui/MainActivity.kt
+++ app/src/main/java/com/example/takeahike/ui/MainActivity.kt
@@ -2,39 +2,27 @@ package com.example.takeahike.ui
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.takeahike.R
-import com.example.takeahike.adapters.StartScreenPagerAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : FragmentActivity() {
 
-    private lateinit var viewPager: ViewPager2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager = findViewById(R.id.viewPager)
-        viewPager.adapter = StartScreenPagerAdapter(this)
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = if (position == 0) {
-                "Current Hike"
-            }
-            else {
-                "Edit Routes"
-            }
-        }.attach()
-    }
-
-    override fun onBackPressed() {
-        if (viewPager.currentItem == 0) {
-            viewPager.
-        }
-        else {
-            viewPager.currentItem = viewPager.currentItem - 1
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+        bottomNav.setOnNavigationItemSelectedListener {
+            it.onNavDestinationSelected(navController) || super.onOptionsItemSelected(it)
         }
     }
 }
