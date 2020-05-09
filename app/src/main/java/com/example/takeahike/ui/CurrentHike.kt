ea0a65e620accfb5164b8c7f666adbf623216ae0
bacowan <bacowan@ualberta.ca>
2020-05-08 22:24:19 -0300

    setting up some configuration

---
+++ app/src/main/java/com/example/takeahike/ui/CurrentHike.kt
@@ -0,0 +1,18 @@
+package com.example.takeahike.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.takeahike.R
+
+class CurrentHike : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.current_hike, container, false)
+    }
+}
--- app/src/main/java/com/example/takeahike/ui/MainActivity.kt
+++ app/src/main/java/com/example/takeahike/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.takeahike
+package com.example.takeahike.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.takeahike.R
 
 class MainActivity : AppCompatActivity() {
 
