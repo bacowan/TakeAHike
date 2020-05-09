9e08ef962124e0336174c546ef044c3df25cdb52
bacowan <bacowan@ualberta.ca>
2020-05-09 16:49:28 -0300

    starting to add in maps

--- app/src/main/java/com/example/takeahike/ui/EditRoute.kt
+++ app/src/main/java/com/example/takeahike/ui/EditRoute.kt
@@ -6,13 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.takeahike.R
+import com.google.android.gms.maps.MapView
 
 class EditRoute : Fragment() {
+    private lateinit var mapView: MapView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.edit_route, container, false)
+        val view = inflater.inflate(R.layout.edit_route, container, false)
+
+        return view
     }
 }
