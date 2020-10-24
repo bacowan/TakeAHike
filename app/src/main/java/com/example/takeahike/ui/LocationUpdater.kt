4e1e037e3783015892bf8d2a569fefd684bffea5
bacowan <bacowan@ualberta.ca>
2020-10-24 20:28:51 -0300

    updates stopped when resuming form backstack

--- app/src/main/java/com/example/takeahike/ui/LocationUpdater.kt
+++ app/src/main/java/com/example/takeahike/ui/LocationUpdater.kt
@@ -26,6 +26,7 @@ class LocationUpdater(
     }
 
     fun endLocationUpdates() {
+        isRequestingLocationUpdates = false
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 }
